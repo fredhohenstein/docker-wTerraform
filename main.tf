73271a5299b63c343ed6cc3b333bab8c1511746b
Fred Hohenstein <fredhohenstein@users.noreply.github.com>
2022-05-30 12:03:22 +0200

    update1
    update1

--- main.tf
+++ main.tf
@@ -7,14 +7,17 @@ terraform {
    }
 }
 
+
 provider "docker" {
     host = "tcp://127.0.0.1:2375"
 }
+
 
 resource "docker_image" "nginx" {
    name = "nginx:latest"
    keep_locally = false
 }
+
 
 resource "docker_container" "nginx" {
    image = docker_image.nginx.latest
